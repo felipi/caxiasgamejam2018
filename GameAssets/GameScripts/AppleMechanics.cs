d99fbd9411329d7aa5d345ba67a4473ff030cd76
Felipi <felipimedeirosmacedo@gmail.com>
2018-07-27 21:42:15 -0300

    Initial commit

---
+++ GameAssets/GameScripts/AppleMechanics.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AppleMechanics : MonoBehaviour {
+	private Rigidbody2D _collider;
+	public float baseTorque;
+	public float baseDrag;
+	public float torqueVariance;
+	public float dragVariance;
+	public bool randomizeTorque;
+	public bool randomizeDrag;
+
+	// Use this for initialization
+	void Start () {
+		_collider = GetComponent<Rigidbody2D>();
+		if(_collider){
+			float torque = baseTorque;
+			float drag = baseDrag;
+			if(randomizeTorque)
+				torque += (Random.value * torqueVariance);
+			if(randomizeDrag)
+				drag += (Random.value * dragVariance);
+			_collider.drag = drag;
+			_collider.AddTorque(torque, ForceMode2D.Force);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ GameAssets/GameScripts/WormMechanics.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WormMechanics : MonoBehaviour {
+	public AppleMechanics parentApple;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		 if (Input.GetMouseButtonDown(0)) {
+			 	Debug.Log("CLICK");
+				 gameObject.transform.parent = null;
+				 Rigidbody2D body = gameObject.GetComponent<Rigidbody2D>();
+				 body.AddForce(transform.up * 20, ForceMode2D.Impulse);
+		 }
+	}
+}
