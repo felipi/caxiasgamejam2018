b61b7da4ed0bee0d5053c4c97cec928e06c69698
Marcos Vinícius Marcon <marconvcm@gmail.com>
2018-07-28 17:14:24 -0300

    Update spawn rulez

--- GameAssets/GameScripts/SpawnMechanics.cs
+++ GameAssets/GameScripts/SpawnMechanics.cs
@@ -1,23 +1,28 @@
-﻿using System.Collections;
+﻿using RoboRyanTron.Unite2017.Variables;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpawnMechanics : MonoBehaviour {
+public class SpawnMechanics : MonoBehaviour
+{
+
+    public IntVariable level;
 
     public float timeToSpawn;
 
     private float currentTime;
 
     // Use this for initialization
-    public void Start ()
+    public void Start()
     {
-		
-	}
+
+    }
 
     // Update is called once per frame
     public void Update()
     {
-        if (currentTime <= 0) {
+        if (currentTime <= 0)
+        {
             this.Spawn();
             this.currentTime = timeToSpawn;
             return;
@@ -30,6 +35,32 @@ public class SpawnMechanics : MonoBehaviour {
     {
         GameObject apple = (GameObject)Instantiate(Resources.Load("Apple"));
         float _x = System.Convert.ToSingle(System.Math.Round(Random.Range(min: -4f, max: 4f)));
+
+        var renderer = apple.GetComponent<SpriteRenderer>();
+         
+        if (Random.Range(1, 100) <= calculate())
+        {
+            renderer.color = Color.yellow;
+        }
+        else
+        {
+            renderer.color = Color.red;
+        }
+
         apple.transform.position = new Vector3(_x, this.transform.position.y, this.transform.position.z);
-    } 
+    }
+
+    private float calculate()
+    {
+        if (level)
+        {
+            if (level.Value > 3)
+            {
+                var result = 20f - ((level.Value - 3f) * 0.5f);
+                if (result < 5.0) return 5.0f;
+                return result;
+            }
+        }
+        return 20f;
+    }
 }
