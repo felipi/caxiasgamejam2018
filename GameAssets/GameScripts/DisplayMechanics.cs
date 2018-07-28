a0133bcac410aae17fd6d4962f9b7c8222dcac16
Marcos Vinícius Marcon <marconvcm@gmail.com>
2018-07-28 10:02:02 -0300

    Finalizacao da mecanica de pontos

---
+++ GameAssets/GameScripts/DisplayMechanics.cs
@@ -0,0 +1,27 @@
+﻿using RoboRyanTron.Unite2017.Variables;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayMechanics : MonoBehaviour
+{
+
+    public Text textElement;
+    public IntVariable actualScore;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (actualScore != null) {
+            var score = actualScore.Value.ToString().PadLeft(5, '0');
+            textElement.text = "Score: " + score;
+        }
+    }
+}
--- GameAssets/GameScripts/ScoreMechanics.cs
+++ GameAssets/GameScripts/ScoreMechanics.cs
@@ -5,18 +5,60 @@ using UnityEngine;
 
 public class ScoreMechanics : MonoBehaviour
 {
+    public int comboColdown;
+    public GameEvent makeScoreEvent;
+    public GameEvent makePerfectScoreEvent;
+    public IntVariable actualScore;
 
-    public IntVariable _actualScore;
+    private int _combo;
+    private float _comboColdown;
+    private int _score;
 
     // Use this for initialization
     void Start()
     {
-
+        _score = 0;
+        _combo = 0;
+        if (actualScore != null) actualScore.SetValue(0);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            this.makeScoreEvent.Raise();
+        }
 
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            this.makePerfectScoreEvent.Raise();
+        }
+
+        if (_comboColdown <= 0)
+        {
+            _comboColdown = 0;
+            _combo = 0;
+        }
+        else
+        {
+            _comboColdown -= Time.deltaTime;
+        }
+    }
+
+    public void MakeScore()
+    {
+        _score += 1;
+
+        if (actualScore != null)
+            actualScore.SetValue(_score);
+    }
+
+    public void MakePerfectScore()
+    {
+        _combo++;
+        _comboColdown = this.comboColdown;
+        this.MakeScore();
+        for (int i = 0; i < _combo; i++) this.MakeScore();
     }
 }
