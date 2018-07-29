1acf638e90cd8391bff02fe939893a8443d0c6d4
Felipi <felipimedeirosmacedo@gmail.com>
2018-07-29 16:30:12 -0300

    Newtown and Perfect Jumps, Spark prefabs

--- GameAssets/GameScripts/AppleCollision.cs
+++ GameAssets/GameScripts/AppleCollision.cs
@@ -8,26 +8,13 @@ public class AppleCollision : MonoBehaviour
     [System.Serializable]
     public class TransferEvent : UnityEvent<MonoBehaviour> { }
     public GameEvent OnCollision;
-    // Use this for initialization
-    void Start()
-    {
 
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
-    void OnTriggerEnter2D(Collider2D coll)
-    {
-        if (coll.gameObject.tag == "Worm")
-        {
+    void OnTriggerEnter2D(Collider2D coll) {
+        if (coll.gameObject.tag == "Worm") {
             MonoBehaviour apple = gameObject.GetComponent<AppleMechanics>();// as MonoBehaviour;
             WormMechanics worm = coll.gameObject.GetComponent<WormMechanics>();
-            if (apple && worm.parentApple != apple)
-            {
+
+            if (apple && worm.parentApple != apple) {
                 OnCollision.Raise(apple);
             }
         }
---
+++ GameAssets/GameScripts/ParticleEmission.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleEmission : MonoBehaviour {
+	public GameObject[] prefabs;
+
+	public void InstantiateParticleSystem(int index){
+		GameObject particles = GameObject.Instantiate(prefabs[index]);
+		particles.transform.position = transform.position;
+	}
+}
