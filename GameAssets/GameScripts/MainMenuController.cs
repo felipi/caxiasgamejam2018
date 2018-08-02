8d806baf86d1625afa0dfaa38f92bd1d455ca2be
Marcos Vinícius Marcon <marconvcm@gmail.com>
2018-08-01 23:05:07 -0300

    Adicionado creditos no menu principal

---
+++ GameAssets/GameScripts/MainMenuController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuController : MonoBehaviour
+{
+    public Button creditsButton;
+    public Canvas creditCanvas;
+    public Button creditCanvasButton;
+
+    // Use this for initialization
+    void Start()
+    {
+        creditCanvas.enabled = false;
+        creditsButton.onClick.AddListener(onCreditsButton);
+        creditCanvasButton.onClick.AddListener(onCreditsButton);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void onCreditsButton()
+    {
+        toggleCredits();
+    }
+
+    void toggleCredits()
+    {
+        creditCanvas.enabled = !creditCanvas.enabled;
+    }
+}
